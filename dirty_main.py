0214aa164de9db1670e0a25f4ca356e45a98b643
Remezov Dmitriy <darknessdizi@gmail.com>
2022-11-23 01:18:19 +0700

    change dirty

--- dirty_main.py
+++ dirty_main.py
@@ -5,6 +5,6 @@ from application.salary import *
 
 if __name__ == '__main__':
     
-    function_call(salary.calculate_salary)
-    function_call(people.get_employees)
-    function_call(salary.name_module, '-- Module function:')
+    function_call(calculate_salary)
+    function_call(get_employees)
+    function_call(name_module, '-- Module function:')
